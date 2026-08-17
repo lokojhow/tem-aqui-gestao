1b7f3eaba40baf24946f887f100e0aec9b1ab0cb
lokojhow <discipulojonathas@gmail.com>
2026-08-17 19:53:49 -0300

    Bump PWA cache after interface redesign

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='tem-aqui-gestao-v0-9-4';
+const CACHE='tem-aqui-gestao-v0-9-5-ui';
 const CORE=['./','./index.html','./styles.css','./app.js','./manifest.json','./supabase-config.js','./gestao-backend.js','./logo-tem-aqui-gestao.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
